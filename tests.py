41904e7c78600f35979a12509b44433cd05d3820
Corey Girard <corey.r.girard@gmail.com>
2017-12-08 00:37:34 -0500

    More tests

--- tests.py
+++ tests.py
@@ -1,6 +1,95 @@
+import shutil, tempfile
+from os import path
 import unittest
 import doctest
 import harbor
+
+
+
+
+class TestFetchFile(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+        with open(path.join(self.test_dir, 'test.py'), 'w') as f:
+            f.write('\n'.join(['aaa',
+                               '    bbb',
+                               '    ccc']))
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_something(self):
+        results = harbor.loadFile(path.join(self.test_dir, 'test.py'))
+
+        expected = ['aaa',
+                    '    bbb',
+                    '    ccc']
+
+        self.assertEqual(results,expected)
+
+
+class TestGetOutline(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+        with open(path.join(self.test_dir, 'test.harbor'), 'w') as f:
+            f.write('\n'.join(['OUTLINE',
+                               'quickstart: quickstart.md',
+                               '  aaa',
+                               '    bbb',
+                               '    ccc',
+                               '  ddd',
+                               '    eee',
+                               'PATTERNS',
+                               'sample:',
+                               '    **{sample}**']))
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_getOutline(self):
+        results = harbor.getOutline(path.join(self.test_dir, 'test.harbor'))
+
+        expected = {'quickstart': {'contents': ['quickstart',
+                                                'quickstart/aaa',
+                                                'quickstart/aaa/bbb',
+                                                'quickstart/aaa/ccc',
+                                                'quickstart/ddd',
+                                                'quickstart/ddd/eee'],
+                                   'filename': 'quickstart.md'}}
+
+        self.assertEqual(results,expected)
+
+class TestGetPatterns(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+        with open(path.join(self.test_dir, 'test.harbor'), 'w') as f:
+            f.write('\n'.join(['OUTLINE',
+                               'quickstart: quickstart.md',
+                               '  aaa',
+                               '    bbb',
+                               '    ccc',
+                               '  ddd',
+                               '    eee',
+                               'PATTERNS',
+                               'sample:',
+                               '    **{sample}**',
+                               'another:',
+                               '    - *`{another}`*']))
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_getPatterns(self):
+        results = harbor.getPatterns(path.join(self.test_dir, 'test.harbor'))
+
+        expected = {'sample': '**{sample}**',
+                    'another': '- *`{another}`*'}
+
+        self.assertEqual(results,expected)
+
+
 
 
 class TestExtractBlockComments(unittest.TestCase):
