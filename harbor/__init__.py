a5875c064b8ba59a4baee3b5fee449d67e527d37
Corey <corey.r.girard@gmail.com>
2017-12-17 17:01:51 -0500

    Deployed to PyPI

---
+++ harbor/__init__.py
@@ -0,0 +1,1 @@
+from .harbor import exe
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='harbor',
+      version='0.1',
+      description='Lightweight documentation generator',
+      url='http://github.com/crgirard/harbor',
+      author='Corey Girard',
+      author_email='corey.r.girard@gmail.com',
+      license='MIT',
+      packages=['harbor'],
+      zip_safe=False)
